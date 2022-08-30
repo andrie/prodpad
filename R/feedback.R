79a815aff1ac8a0b723a7f0f3ed4123d0da74cd3
andrie <apdevries@gmail.com>
2022-08-30 11:49:22 +0000

    Add paging to pp_get_feedback()

--- R/feedback.R
+++ R/feedback.R
@@ -1,40 +1,60 @@
 
 #' Get tibble of feedbacks.
 #'
-#' Returns a tibble of feedback which can be a straight list or grouped by contact using the group_by parameter.  The list can also be filtered by state query parameter for active, archived, unsorted or all.Note that by default only active feedback is returned.
+#' Returns a tibble of feedback which can be a straight list or grouped by
+#' contact using the group_by parameter.  The list can also be filtered by state
+#' query parameter for active, archived, unsorted or all.Note that by default
+#' only active feedback is returned.
 #'
 #'
 #' @param group_by Setting to customer will group the feedback by customer
 #'
-#' @param state Set to value of `active` for active, `archived` for archived feedback, `unsorted` for unsorted feedback and `all` for all feedback. Default is `active`.
+#' @param state Set to value of `active` for active, `archived` for archived
+#'   feedback, `unsorted` for unsorted feedback and `all` for all feedback.
+#'   Default is `all`.
 #'
 #' @param page Set to page through the results. Default is 1.
 #'
 #' @param size For setting the number of results per page. Default is 100.
 #'
-#' @param company Set to filter the feedback results based on whether the feedback was entered for a contact linked to the company.
+#' @param company Set to filter the feedback results based on whether the
+#'   feedback was entered for a contact linked to the company.
 #'
-#' @param company_country Set to filter the feedback results based on the country set for the associated company.
+#' @param company_country Set to filter the feedback results based on the
+#'   country set for the associated company.
 #'
-#' @param company_size Set to filter the feedback results based on the size of the company for the associated company.
+#' @param company_size Set to filter the feedback results based on the size of
+#'   the company for the associated company.
 #'
-#' @param company_value Set to filter the feedback results based on the value of the company for the company assocaited to the feedback.
+#' @param company_value Set to filter the feedback results based on the value of
+#'   the company for the company assocaited to the feedback.
 #'
 #' @param customer Can be either numeric ID, UUID of a contac or contact email.
 #'
-#' @param product Filter results by the product associated to feedback. Can either be the product UUID or product ID.
+#' @param product Filter results by the product associated to feedback. Can
+#'   either be the product UUID or product ID.
 #'
-#' @param persona Filter results by the persona associated to feedback. Can either be the persona UUID or persona ID.
+#' @param persona Filter results by the persona associated to feedback. Can
+#'   either be the persona UUID or persona ID.
 #'
-#' @param job_role Filter results by the job role of the contact associated to the feedback. Use the JobRole UUID.
+#' @param job_role Filter results by the job role of the contact associated to
+#'   the feedback. Use the JobRole UUID.
 #'
-#' @param tags Filter feedback results by the tags associated to the feedback. Mulitple tags can be specified and acts as an OR. Use the tag ID or UUID.
+#' @param tags Filter feedback results by the tags associated to the feedback.
+#'   Mulitple tags can be specified and acts as an OR. Use the tag ID or UUID.
 #'
-#' @param has_ideas Whether the feedback is associated to one or more ideas. Default is either.
+#' @param has_ideas Whether the feedback is associated to one or more ideas.
+#'   Default is either.
 #'
-#' @param external_id Filter feedback to return the feedback associated with a specific External ID. An example of an external ID is the ID of a record in a CRM or ID of a ticket in a customer support application.
+#' @param external_id Filter feedback to return the feedback associated with a
+#'   specific External ID. An example of an external ID is the ID of a record in
+#'   a CRM or ID of a ticket in a customer support application.
 #'
-#' @param external_url Filter feedback to return the feedback associated with a specific external url. An example of an external url is that of a record in a CRM or a ticket in a customer support application
+#' @param external_url Filter feedback to return the feedback associated with a
+#'   specific external url. An example of an external url is that of a record in
+#'   a CRM or a ticket in a customer support application
+#'
+#' @param .limit The maximum number of elements to return
 #'
 #' @param ... Other arguments passed to [.pp()]()]
 #'
@@ -44,7 +64,7 @@
 #'
 #' @export
 pp_get_feedbacks <- function(
-    state = c("active", "archived", "unsorted", "all"),
+    state = c("all", "active", "archived", "unsorted"),
     company = NULL,
     company_country = NULL,
     company_size = NULL,
@@ -60,36 +80,73 @@ pp_get_feedbacks <- function(
     group_by = FALSE,
     page = 1,
     size = 100,
+    .limit = NULL,
     ...
 ) {
   "state" <- match.arg(state)
- .pp("get /feedbacks",
-     group_by = group_by,
-     state = state,
-     page = page,
-     size = size,
-     company = company,
-     company_country = company_country,
-     company_size = company_size,
-     company_value = company_value,
-     customer = customer,
-     product = product,
-     persona = persona,
-     job_role = job_role,
-     tags = tags,
-     has_ideas = has_ideas,
-     external_id = external_id,
-     external_url = external_url,
-     ... = ...,
-     .unnest = TRUE,
-     .unnest_dont_unlist = c("tags", "ideas", "products", "personas")
-  )
+
+  get_one <- function(page, size) {
+    .pp("get /feedbacks",
+        group_by = group_by,
+        state = state,
+        page = page,
+        size = size,
+        company = company,
+        company_country = company_country,
+        company_size = company_size,
+        company_value = company_value,
+        customer = customer,
+        product = product,
+        persona = persona,
+        job_role = job_role,
+        tags = tags,
+        has_ideas = has_ideas,
+        external_id = external_id,
+        external_url = external_url,
+        ... = ...,
+        .unnest = TRUE,
+        .unnest_dont_unlist = c("tags", "ideas", "products", "personas")
+    )
+  }
+
+
+  get_all <- function(f, page = page, size = size, .limit = .limit) {
+    if (is.null(.limit)) .limit <- size
+    if (.limit < size) size <- .limit
+
+    # get the first tibble
+    z <- match.fun(f)(page = page, size = size)
+    print(glue::glue("> Page: {page}, nrow(z): {nrow(z)})"))
+    if (nrow(z) == 0) return(z)
+
+    if (nrow(z) == 0) return(z)
+    .returned <- if (is.na(get_count(z))) .returned <- nrow(z)
+    .count <- min(get_count(z), .limit, na.rm = TRUE)
+
+    # get all subsequent tibbles
+    while (nrow(z) < max(.limit, .returned) && .returned == size) {
+      page <- page + 1
+      print(glue::glue("> Page: {page}, nrow(z): {nrow(z)})"))
+      if(.limit < Inf) size <- min(size, .limit - nrow(z))
+      z2 <- match.fun(f)(page = page, size = size)
+      if (nrow(z2) == 0) return(z)
+      z <- dplyr::bind_rows(z, z2)
+    }
+    z
+  }
+
+  get_all(get_one, page = page, size = size, .limit = .limit)
 }
 
 
+get_count <- function(x) {
+  attr(x, "count")
+}
+
 #' Get a piece of feedback.
 #'
-#' Return an individual piece of feedback. The feedback is returned with the details of the contact that provided the piece of feedback.
+#' Return an individual piece of feedback. The feedback is returned with the
+#' details of the contact that provided the piece of feedback.
 #'
 #'
 #' @param id Feedback ID.
@@ -183,7 +240,10 @@ pp_post_feedback <- function(
 
 #' Archive an existing piece of feedback.
 #'
-#' Use this endpoint to edit the details of an existing piece of feedback including the feedback, the status and external links to add (for example link to a record in the CRM, other 3rd party application or video of the customer interview).
+#' Use this endpoint to edit the details of an existing piece of feedback
+#' including the feedback, the status and external links to add (for example
+#' link to a record in the CRM, other 3rd party application or video of the
+#' customer interview).
 #'
 #'
 #' @param id Feedback ID
