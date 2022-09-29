5b58ad0473ac2548f7cb5d6534674e3611c2905b
andrie <apdevries@gmail.com>
2022-09-29 16:29:38 +0000

    Catch and deal with NULL columns

--- R/unnest_tibble.R
+++ R/unnest_tibble.R
@@ -1,3 +1,9 @@
+unnull_and_tibble <- function(x) {
+  null_to_na <- function(x) if(length(x)) x else NA
+  map(x, ~map(., null_to_na)) %>% map_dfr(as_tibble)
+}
+
+
 #' Unnest a list column of tibbles.
 #'
 #' This is frequently necessary to unnest `tags`, `products`, `feedbacks`, `personas` and other columns embedded in prodpad data.
@@ -14,7 +20,10 @@
 #' @importFrom tidyr unnest_longer unnest_wider
 unnest_tibble <- function(.data, .col) {
   .data %>%
-    mutate({{ .col }} := {{ .col }} %>%  map(~map_dfr(., as_tibble))) %>%
+    mutate({{ .col }} := {{ .col }} %>%  map(unnull_and_tibble)) %>%
     unnest_longer({{ .col }}) %>%
-    unnest_wider({{ .col }}, names_sep = "_")
+    unnest_wider({{ .col }}, names_sep = "_") %>%
+    identity()
 }
+
+
