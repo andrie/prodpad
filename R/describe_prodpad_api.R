1a8ef5ba95575a0742b9493b0d9d83cd17f70913
andrie <apdevries@gmail.com>
2025-04-12 13:16:35 +0100

    Refactoring

--- R/describe_prodpad_api.R
+++ R/describe_prodpad_api.R
@@ -33,7 +33,8 @@ describe_api <- function(max.level = 4) {
 describe_api_paths <- function(search = "") {
   path <- NULL # for R CMD check
   prodpad_api_v1$paths |>
-    purrr::map_dfr(~ tibble::tibble(verb = names(.)), .id = "path") |>
+    map(~ tibble::tibble(verb = names(.)), .id = "path") |>
+    list_rbind() |>
     dplyr::filter(grepl(search, path)) |>
     identity()
 }
